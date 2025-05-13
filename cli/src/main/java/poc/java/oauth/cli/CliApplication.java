899692720a5284fd544a2dcd5dd96196b60109a9
Fabrice B <fabrice.bibonne@insee.fr>
2025-05-13 09:17:18 +0200

    feat (authorization code flow and spnego without servlet context) WIP - spring shell commands instantiated from open api spec - inject RestClient from spring-oauth-cli module

--- cli/src/main/java/poc/java/oauth/cli/CliApplication.java
+++ cli/src/main/java/poc/java/oauth/cli/CliApplication.java
@@ -1,6 +1,8 @@
 package poc.java.oauth.cli;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -8,13 +10,21 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2Clien
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesMapper;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.shell.command.CommandRegistration;
 import org.springframework.shell.command.CommandResolver;
 import org.springframework.shell.command.annotation.CommandScan;
+import org.springframework.web.client.RestClient;
 import poc.java.oauth.EnableRestClientWithoutServlet;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.Collection;
+
+import static org.springframework.shell.command.CommandRegistration.OptionArity.EXACTLY_ONE;
+import static org.springframework.shell.command.CommandRegistration.OptionArity.ZERO_OR_ONE;
 
 @SpringBootApplication
 @CommandScan
@@ -32,18 +42,31 @@ public class CliApplication {
         return (new OAuth2ClientPropertiesMapper(properties)).asClientRegistrations().values();
     }
 
-    //API RH ? (trombi)
     @Bean
-    CommandResolver commands(OpenAPI openAPI){
-        var baseUri = openAPI.getServers().getFirst().getUrl();
+    OpenAPI openAPI(@Value("${poc.java.oauth.openapi.spec.resource}")String openapiSpecResource, ResourceLoader resourceLoader) throws IOException {
+        Resource resource = resourceLoader.getResource(openapiSpecResource);
+        return (new OpenAPIV3Parser()).read(resource.getURI().toString());
+    }
+
+
+    @Bean
+    CommandResolver commands(OpenAPI openAPI, RestClient.Builder restCLientBuilder) {
+        final URI baseUri = URI.create(openAPI.getServers().getFirst().getUrl());
         return () -> openAPI.getPaths().entrySet().stream()
-                .flatMap(CliCommand::fromPath)
-                .map(cliCommand -> CommandRegistration.builder()
-                        .command(cliCommand.id())
-                        .withTarget().function(cliCommand)
-                        .and().build())
+                .flatMap(entryPath -> CliCommand.fromPath(entryPath, baseUri, restCLientBuilder))
+                .map(cliCommand -> {
+                    CommandRegistration.Builder builder = CommandRegistration.builder();
+                    builder.command(cliCommand.id()).withTarget().function(cliCommand);
+                    cliCommand.parameters().forEach(urlParameter -> builder.withOption()
+                            .longNames(urlParameter.name())
+                            .arity(urlParameter.required()?EXACTLY_ONE:ZERO_OR_ONE)
+                    );
+                    return builder.build();
+                })
                 .toList();
     }
+
+
 
 
 }
--- cli/src/main/java/poc/java/oauth/cli/CliCommand.java
+++ cli/src/main/java/poc/java/oauth/cli/CliCommand.java
@@ -2,23 +2,58 @@ package poc.java.oauth.cli;
 
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import org.springframework.shell.command.CommandContext;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public record CliCommand(String id) implements Function<CommandContext, String> {
+public record CliCommand(String id, OperationMethod operationMethod, RestClient restClient, Set<UrlParameter> parameters) implements Function<CommandContext, String> {
 
-    private static final Function<PathItem, Operation>[] toOperations = {PathItem::getDelete,};
+    CliCommand(String id, OperationMethod operationMethod, UriComponentsBuilder uriBuilder, Set<UrlParameter> parameters, RestClient.Builder restCLientBuilder){
+        this(id, operationMethod, restCLientBuilder.uriBuilderFactory(new DefaultUriBuilderFactory(uriBuilder)).build(), parameters);
+    }
+
 
     @Override
     public String apply(CommandContext commandContext) {
         return "to be implemented";
     }
 
-    public static Stream<CliCommand> fromPath(Map.Entry<String, PathItem> pathEntry) {
-        String uriSuffix = pathEntry.getKey();
-        pathEntry.getValue().getDelete().getOperationId()
+    public static Stream<CliCommand> fromPath(Map.Entry<String, PathItem> pathEntry, URI serverUri, RestClient.Builder restCLientBuilder) {
+        final String uriSuffix = pathEntry.getKey();
+        return Stream.of(OperationMethod.values())
+                        .map(operationMethod -> CliCommand.of(serverUri, uriSuffix, pathEntry.getValue(), operationMethod, restCLientBuilder));
+
+    }
+
+    private static CliCommand of(URI serverUri, String uriSuffix, PathItem pathItem, OperationMethod operationMethod, RestClient.Builder restCLientBuilder) {
+        Operation operation = operationMethod.toOperation.apply(pathItem);
+        var id = operation.getOperationId();
+        return new CliCommand(id, operationMethod, UriComponentsBuilder.fromUri(serverUri).path(uriSuffix),
+                operation.getParameters().stream().map(UrlParameter::new).collect(Collectors.toSet()), restCLientBuilder);
+    }
+
+    record UrlParameter(String name, boolean required) {
+        UrlParameter(Parameter parameter){
+            this(parameter.getName(), parameter.getRequired());
+        }
+    }
+
+    enum OperationMethod {
+        GET(PathItem::getGet), POST(PathItem::getPost), PUT(PathItem::getPut), PATCH(PathItem::getPatch), DELETE(PathItem::getDelete);
+
+        private final Function<PathItem, Operation> toOperation;
+
+        OperationMethod(Function<PathItem, Operation> toOperation) {
+            this.toOperation = toOperation;
+        }
     }
 }
