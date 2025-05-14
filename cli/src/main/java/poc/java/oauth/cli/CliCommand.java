a66e231f8d03c0541546d0c9783a4e0537bec465
Fabrice B <fabrice.bibonne@insee.fr>
2025-05-14 13:54:24 +0200

    feat (authorization code flow and spnego without servlet context) WIP - call API with RestClient

--- cli/src/main/java/poc/java/oauth/cli/CliCommand.java
+++ cli/src/main/java/poc/java/oauth/cli/CliCommand.java
@@ -3,6 +3,8 @@ package poc.java.oauth.cli;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.parameters.Parameter;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.http.HttpMethod;
 import org.springframework.shell.command.CommandContext;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
@@ -10,50 +12,77 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public record CliCommand(String id, OperationMethod operationMethod, RestClient restClient, Set<UrlParameter> parameters) implements Function<CommandContext, String> {
+public record CliCommand(String id, OperationMethod operationMethod, RestClient restClient,
+                         Set<UrlParameter> parameters) implements Function<CommandContext, String> {
 
-    CliCommand(String id, OperationMethod operationMethod, UriComponentsBuilder uriBuilder, Set<UrlParameter> parameters, RestClient.Builder restCLientBuilder){
+    CliCommand(String id, OperationMethod operationMethod, UriComponentsBuilder uriBuilder, Set<UrlParameter> parameters, RestClient.Builder restCLientBuilder) {
         this(id, operationMethod, restCLientBuilder.uriBuilderFactory(new DefaultUriBuilderFactory(uriBuilder)).build(), parameters);
     }
 
 
     @Override
     public String apply(CommandContext commandContext) {
-        return "to be implemented";
+        return restClient.method(operationMethod.restClientMethod())
+                .uri("/",
+                        parameters.stream()
+                                .<Pair<String, String>>mapMulti((parameter, downstream) -> {
+                                    String value = commandContext.getOptionValue(parameter.name());
+                                    if (value != null) {
+                                        downstream.accept(Pair.of(parameter.name(), value));
+                                    }
+                                    if (parameter.required()) {
+                                        throw new IllegalStateException("Required parameter " + parameter.name() + " is not present");
+                                    }
+                                })
+                                .collect(Collectors.toMap(Pair::getKey, Pair::getValue))
+                ).retrieve()
+                .body(String.class);
+
     }
 
     public static Stream<CliCommand> fromPath(Map.Entry<String, PathItem> pathEntry, URI serverUri, RestClient.Builder restCLientBuilder) {
         final String uriSuffix = pathEntry.getKey();
         return Stream.of(OperationMethod.values())
-                        .map(operationMethod -> CliCommand.of(serverUri, uriSuffix, pathEntry.getValue(), operationMethod, restCLientBuilder));
+                .mapMulti((operationMethod, downstream) -> CliCommand.of(serverUri, uriSuffix, pathEntry.getValue(), operationMethod, restCLientBuilder)
+                        .ifPresent(downstream));
 
     }
 
-    private static CliCommand of(URI serverUri, String uriSuffix, PathItem pathItem, OperationMethod operationMethod, RestClient.Builder restCLientBuilder) {
+    private static Optional<CliCommand> of(URI serverUri, String uriSuffix, PathItem pathItem, OperationMethod operationMethod, RestClient.Builder restCLientBuilder) {
         Operation operation = operationMethod.toOperation.apply(pathItem);
+        if (operation == null) {
+            return Optional.empty();
+        }
         var id = operation.getOperationId();
-        return new CliCommand(id, operationMethod, UriComponentsBuilder.fromUri(serverUri).path(uriSuffix),
-                operation.getParameters().stream().map(UrlParameter::new).collect(Collectors.toSet()), restCLientBuilder);
+        return Optional.of(new CliCommand(id, operationMethod, UriComponentsBuilder.fromUri(serverUri).path(uriSuffix),
+                operation.getParameters().stream().map(UrlParameter::new).collect(Collectors.toSet()), restCLientBuilder));
     }
 
     record UrlParameter(String name, boolean required) {
-        UrlParameter(Parameter parameter){
+        UrlParameter(Parameter parameter) {
             this(parameter.getName(), parameter.getRequired());
         }
     }
 
     enum OperationMethod {
-        GET(PathItem::getGet), POST(PathItem::getPost), PUT(PathItem::getPut), PATCH(PathItem::getPatch), DELETE(PathItem::getDelete);
+        GET(PathItem::getGet, HttpMethod.GET), POST(PathItem::getPost, HttpMethod.POST), PUT(PathItem::getPut, HttpMethod.PUT), PATCH(PathItem::getPatch, HttpMethod.PATCH), DELETE(PathItem::getDelete, HttpMethod.DELETE);
 
         private final Function<PathItem, Operation> toOperation;
+        private final HttpMethod restClientMethod;
 
-        OperationMethod(Function<PathItem, Operation> toOperation) {
+        OperationMethod(Function<PathItem, Operation> toOperation, HttpMethod restClientMethod) {
             this.toOperation = toOperation;
+            this.restClientMethod = restClientMethod;
+        }
+
+        public HttpMethod restClientMethod() {
+            return this.restClientMethod;
         }
     }
 }
